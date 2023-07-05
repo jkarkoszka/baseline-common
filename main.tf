3a85a12cf8b9dcac3ab44cde261b64d7d04a0598
Jakub Karkoszka <jakub.karkoszka@wipro.com>
2023-07-05 15:48:05 +0200

    4# commit

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 locals {
   module_name_region_and_account_name = "baseline-common-${var.region}-${var.account_name}"
-  result = "${local.module_name_region_and_account_name} ++ ${depedencies_result}"
+  result = "${local.module_name_region_and_account_name} ++ ${var.depedencies_result}"
 }
